d786986ab12af18b05189532d55d3354dc85950a
dulquer9 <73124813+dulquer9@users.noreply.github.com>
2021-02-04 12:09:53 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,32 +1,18 @@
 class script(object):
-    START_TEXT ="""<b>Hello User,
-
-A Simple File Renamer Bot With Permanent Thumbnail support!💯
-
-Send me any Telegram file and choose appropriate option!
-
-Click /help for more details....</b>"""
+    START_TEXT = """ A Simple File Renamer Bot With Permanent Thumbnail support!💯
+<b>Send me any Telegram file and choose appropriate option! </b>"""
 
     RENAME_403_ERR = "What Are You Doing? You are Banned"
-    UPGRADE_TEXT = "CONTACT @RichardMadden_09"
+    UPGRADE_TEXT = "CONTACT @prgofficial"
     DOWNLOAD_START = "Give Me Some Time..."
     UPLOAD_START = "Starting to upload..."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "Thank you for Using Meh!! ❤️"
-    SAVED_THUMB = "✅Custom thumbnail Saved."
-This thumbnail will be Permanent for all future uploads
-
- Do /delthumb to clear your thumbnail!"
-    DEL_THUMB = "Thumbnail cleared succesfully ✅"
-    NO_THUMB = "SED😕 No saved thumbnails Found!!"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me > ©  @prgofficial **"
+    SAVED_THUMB = "Thumbnail Saved ✅ This Is Permanent"
+    DEL_THUMB = "Thumbnail cleared succesfully!"
+    NO_THUMB = "No thumbnails found!"
     SAVED_RECVD_DOC_FILE = "File Downloaded Successfully 😎"
     CUSTOM_CAPTION_UL_FILE = " "
-    HELP_USER = """<b>Hey.. It's not that complicated😅
-
-Follow These steps..</b>
+    HELP_USER = """It's not that complicated😅
     
-🌀 Send an image and it will be saved as your permanent Thumbnail
-
-🌀 Now send your file
-
-🌀 Select rename option and enter your new name ( extension is not required )"""
-
+1. Send me any Telegram File.
+2. Choose appropriate option."""
